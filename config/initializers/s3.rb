796372763670b79dbbac6461cebc54b03432853e
phong.hoang <phong.hoang@linkbal.co.jp>
2022-12-25 11:14:06 +0700

    detele config s3 file

--- config/initializers/s3.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-    config.fog_credentials = {
-        :provider               => '',
-        :aws_access_key_id      => "",
-        :aws_secret_access_key  => "",
-        :region                 => '' # Change this for different AWS region. Default is 'us-east-1'
-    }
-    config.fog_directory  = "video-music-system"
-  end
