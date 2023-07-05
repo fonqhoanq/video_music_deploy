b74b8aadd6ca94ac02e74f8bb8af0f1855a1e9d3
phong.hoang <phong.hoang@linkbal.co.jp>
2023-07-05 22:10:18 +0700

    fix avatar URL

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+  :jwt_authenticatable,
+  jwt_revocation_strategy: JwtDenylist
   has_many :subscribes
   has_many :feelings
   has_many :replies
@@ -14,10 +17,7 @@ class User < ApplicationRecord
   }
   validates :name, length: {minimum:3, maximum:40}
   validates :age, numericality: { only_numeric: true }
-  devise :database_authenticatable, :registerable,
-         :jwt_authenticatable,
-         jwt_revocation_strategy: JwtDenylist
-  # def self.ransackable_attributes(auth_object = nil)
-  #   ["age", "authentication_token", "created_at", "email", "encrypted_password", "id", "gender", "name", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
-  # end
+  def self.ransackable_attributes(auth_object = nil)
+    ["age", "authentication_token", "created_at", "email", "encrypted_password", "id", "gender", "name", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
+  end
 end
